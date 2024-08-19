2e1e0f88e19f1d156e1971411fb8b6a7f4d0dfb5
redskyr <134505483+redskyr@users.noreply.github.com>
2024-08-19 18:45:55 -0300

    update source/TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -410,7 +410,7 @@ class TitleState extends MusicBeatState
 
 		var pressedEnter:Bool = FlxG.keys.justPressed.ENTER || controls.ACCEPT;
 
-		#if mobile
+		#if android
 		for (touch in FlxG.touches.list)
 		{
 			if (touch.justPressed)
